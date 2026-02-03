fc9a936e766f7b4c93d1a3df698897d4a689ac77
PSH <psh9408p@naver.com>
2026-02-03 23:26:12 +0900

    feat: icon 설정 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import EmptyIcon from "@/assets/icons/empty.svg?react";
+import FillIcon from "@/assets/icons/fill.svg?react";
+
 function App() {
-    return <></>;
+    return (
+        <div className="flex items-center gap-6 p-6">
+            <EmptyIcon width={22} height={20} aria-hidden />
+            <FillIcon width={24} height={24} aria-hidden />
+        </div>
+    );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        svgr(),
         react({
             babel: {
                 plugins: [["babel-plugin-react-compiler"]],
             },
         }),
     ],
+    resolve: {
+        alias: { "@": path.resolve(__dirname, "./src") },
+    },
 });
